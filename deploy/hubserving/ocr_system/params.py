a86e7425bd734a6712caa95c27be4fdb6fe0f587
happyguozf <happyguozf@gmail.com>
2021-12-02 21:00:51 +0800

    Update params.py

--- deploy/hubserving/ocr_system/params.py
+++ deploy/hubserving/ocr_system/params.py
@@ -13,7 +13,7 @@ def read_params():
 
     #params for text detector
     cfg.det_algorithm = "DB"
-    cfg.det_model_dir = "./inference/ch_ppocr_server_v2.0_det_infer/"
+    cfg.det_model_dir = "./inference/ch_ppocr_mobile_v2.0_det_infer/"
     cfg.det_limit_side_len = 960
     cfg.det_limit_type = 'max'
 
@@ -31,14 +31,14 @@ def read_params():
 
     #params for text recognizer
     cfg.rec_algorithm = "CRNN"
-    cfg.rec_model_dir = "./inference/ch_ppocr_server_v2.0_rec_infer/"
+    cfg.rec_model_dir = "./inference/en_number_mobile_v2.0_rec_infer/"
 
     cfg.rec_image_shape = "3, 32, 320"
-    cfg.rec_char_type = 'ch'
+    cfg.rec_char_type = 'EN'
     cfg.rec_batch_num = 30
     cfg.max_text_length = 25
 
-    cfg.rec_char_dict_path = "./ppocr/utils/ppocr_keys_v1.txt"
+    cfg.rec_char_dict_path = ".ppocr/utils/en_dict.txt"
     cfg.use_space_char = True
 
     #params for text classifier
